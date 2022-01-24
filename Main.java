017629cc612b155af3b9a95c1606bde0e191f4f7
Vladimir Levin <94986766+francLv@users.noreply.github.com>
2022-01-24 11:12:32 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+        int z = cube(5);
+        System.out.println("5 в кубе: " + z);
+    }
+    public static int cube(int x) {
+        return x * x * x;
+  }
+}
